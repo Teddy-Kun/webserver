3fb7784a4667561f6690b9ae8bdaeae65af8be56
Paul <58993831+Teddy-Kun@users.noreply.github.com>
2026-02-13 10:22:20 +0100

    docs: more detailed comment

--- cpp/src/error.hpp
+++ cpp/src/error.hpp
@@ -14,12 +14,13 @@ class [[nodiscard]] Error {
 		HttpCode code;
 	};
 
-	// having data in an internal struct behind a pointer means that error is
-	// only 8 bytes on the stack the reduced size means that if it is used in
-	// std::expected, the size of the result is as small as possible this
-	// increases that chance that our data is passed in registers, instead on
-	// the stack tldr, its an optimization for the happy path, that accepts heap
-	// memory allocation in case of an error
+	// Having data in an internal struct behind a pointer means that error is
+	// only 8 bytes on the stack. The reduced size means that if it is used in
+	// std::expected, the size of the result is as small as possible. This
+	// increases the chance that our data is passed in registers, instead of on
+	// the stack. TLDR, its an optimization for the happy path, that accepts heap
+	// memory allocation in case of an error. Also allows for very easy, simple
+	// and fast move assignments.
 	std::unique_ptr<Data> ptr;
 
 	[[nodiscard]] auto to_string() const noexcept -> std::string;
